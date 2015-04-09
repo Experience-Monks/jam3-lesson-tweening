b19b410a494dae3c9137ec7f63b48b734267def1
Jonathan McKinnell <jmckinnell@gmail.com>
2015-04-09 13:30:02 -0400

    updates

--- lessons/2- Animating the DOM/practice.js
+++ lessons/2- Animating the DOM/practice.js
@@ -8,20 +8,35 @@ var toTween = document.createElement('button');
 var elOut = document.createElement('pre');
 var startTime = Date.now();
 
+
+
 var engine = rafLoop( function(dt) {
+
+	var propertiesToDisplay = [
+		toTween.style.transform,
+		toTween.style.opacity
+	];
+		
 	// tweenlite adds a wierd property called _gsTweenID we'll just temporarily remove it
 	var tweenID = toTween._gsTweenID;
 	delete toTween._gsTweenID;
-	elOut.innerHTML = JSON.stringify(toTween, undefined, '  ');
+	elOut.innerHTML = JSON.stringify(propertiesToDisplay, undefined, '  ');
 	toTween._gsTweenID = tweenID;
 });
 
-gsap.set(toTween, {bgColor: '#333333', color: "#ffffff"});
+toTween.innerHTML = 'Awesome sauce!';
+gsap.set(toTween, {
+	backgroundColor: '#333333',
+	border: "1px solid #000",
+	color: "#ffffff", 
+	fontSize: "20px",
+	padding: "7px",
+	borderRadius: "5px 5px 5px 5px"
+});
 
 document.body.appendChild(elOut);
 document.body.appendChild(toTween);
 engine.start();
-
 
 
 test('Testing your tween', function(t) {
--- lessons/2- Animating the DOM/practice/index.js
+++ lessons/2- Animating the DOM/practice/index.js
@@ -2,8 +2,8 @@ var Tween = require('gsap');
 
 // In the following function animate a button
 // The values it should animate are transformX from -40 to 0, transformY from -30 to 0, and opacity from 0 to 1.
-// It should complete in 0.75 seconds.
+// It should complete in 0.75 seconds. WOAH!
 // Once the tween has finished call the `onTweenFinished` function
 module.exports = function(toTween, onTweenFinished) {
-
+	Tween.fromTo(toTween, 0.75, {x: -40, y: -30, autoAlpha: 0}, {x: 0, y: 0, autoAlpha: 1, onComplete: onTweenFinished });
 };
