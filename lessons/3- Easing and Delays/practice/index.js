96559574cd3e5733f59896d3994804e0a08eaf7f
Jonathan McKinnell <jmckinnell@gmail.com>
2015-04-29 12:03:58 -0400

    Fixing 3rd Lesson

--- lessons/3- Easing and Delays/practice/index.js
+++ lessons/3- Easing and Delays/practice/index.js
@@ -1,13 +1,16 @@
 var Tween = require('gsap');
 
-// In the following function animate a button
-// The values it should animate are:
-// 	1. x from -40 to 0
-// 	2. y from -30 to 0
-// 	3. opacity from 0 to 1.
-// 	4. visibility should be inherit
-// It should complete in 1 seconds.
-// Once the tween has finished call the `onTweenFinished` function
+// For the following practice we're going to Tween 3 objects.
+// Here are the rules.
+// 1. All 3 objects must finish their tween in 3 seconds
+// 2. The first tween for 'toTween.object1' should have duration of 2 seconds.
+// 3. The second tween for 'toTween.object2' should have a duration of 1.3 seconds.
+// 4. The second tween for 'toTween.object3' should have a durction of 0.5 seconds.
+// 5. All 3 objects should use the Quint ease.
+// 6. Each Tween needs to call thier respetive onTweenFnished calls. (i.e. onTweenFinished.object1)
+
+
+// Once the 3rd tween has finished call the `onTweenFinished` function
 module.exports = function(toTween, onTweenFinished) {
 
 };
